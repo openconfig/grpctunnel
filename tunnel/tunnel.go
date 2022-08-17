3c7e8a0534c0d633c991cdad9f90a1dd98366f20
Carl Lebsack <csl@google.com>
2022-08-17 19:51:23 -0400

    Avoid nil pointer dereference in server without a RegisterHandler configured.

--- tunnel/tunnel.go
+++ tunnel/tunnel.go
@@ -819,10 +819,13 @@ func (s *Server) newClientSession(ctx context.Context, session *tpb.Session, add
 	// If client is requesting a remote target, only call the bridge register handler.
 	// We might extend it to allow calling the customized handler in the future.
 	tc := s.clientFromTarget(t)
-	if tc != nil {
+	switch {
+	case tc != nil:
 		err = s.bridgeRegHandler(ServerSession{addr, t})
-	} else {
+	case s.sc.RegisterHandler != nil:
 		err = s.sc.RegisterHandler(ServerSession{addr, t})
+	default:
+		err = fmt.Errorf("no target %q of type %q registered", session.Target, session.TargetType) 
 	}
 	if err != nil {
 		if err := rs.Send(&tpb.RegisterOp{Registration: &tpb.RegisterOp_Session{Session: &tpb.Session{Tag: tag, Error: err.Error()}}}); err != nil {
