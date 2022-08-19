19090616647558f8196c4f3e10186823c8819596
Carl Lebsack <csl@google.com>
2022-08-19 09:39:09 -0400

    Same nil pointer handling for Handler

--- tunnel/tunnel.go
+++ tunnel/tunnel.go
@@ -867,10 +867,13 @@ func (s *Server) newClientSession(ctx context.Context, session *tpb.Session, add
 			// We might extend it to allow calling the customized handler in the future.
 			var err error
 			tc := s.clientFromTarget(t)
-			if tc != nil {
+			switch {
+			case tc != nil:
 				err = s.bridgeHandler(ServerSession{addr, t}, ioe.rwc)
-			} else {
+			case s.sc.Handler != nil:
 				err = s.sc.Handler(ServerSession{addr, t}, ioe.rwc)
+			default:
+				err = fmt.Errorf("no target %q of type %q registered", session.Target, session.TargetType)
 			}
 			if err != nil {
 				s.sendError(err)
